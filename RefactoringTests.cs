7e9a2847ee9bb0e4a9e04856543cb598b3702fb5
DRed <dima.redkin.1999@gmail.com>
2020-04-01 12:41:22 +0500

    100% test code coverage

--- RefactoringTests.cs
+++ RefactoringTests.cs
@@ -33,11 +33,114 @@ namespace Refactoring
                     "Contrast 20"
                 });
         }
-        
-        
 
         [Test]
-        public void BrightnessUp()
+        public void TurnOnTv()
+        {
+            TestRemoteController(new[]
+                {
+                    "Tv On"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline True",
+                    "Volume 30",
+                    "Brightness 20",
+                    "Contrast 20"
+                });
+        }
+
+        [Test]
+        public void TurnOffTv()
+        {
+            TestRemoteController(new[]
+                {
+                    "Tv On",
+                    "Tv Off"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline False",
+                    "Volume 30",
+                    "Brightness 20",
+                    "Contrast 20"
+                });
+        }
+
+        [Test]
+        public void UpVolume()
+        {
+            TestRemoteController(new[]
+                {
+                    "Tv On",
+                    "Volume Up"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline True",
+                    "Volume 40",
+                    "Brightness 20",
+                    "Contrast 20"
+                });
+        }
+
+        [Test]
+        public void DownVolume()
+        {
+            TestRemoteController(new[]
+                {
+                    "Tv On",
+                    "Volume Down"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline True",
+                    "Volume 20",
+                    "Brightness 20",
+                    "Contrast 20"
+                });
+        }
+
+        [Test]
+        public void DontUpVolume_When_TvIsOff()
+        {
+            TestRemoteController(new[]
+                {
+                    "Volume Up"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline False",
+                    "Volume 30",
+                    "Brightness 20",
+                    "Contrast 20"
+                });
+        }
+
+        [Test]
+        public void DontDownVolume_When_TvIsOff()
+        {
+            TestRemoteController(new[]
+                {
+                    "Volume Down"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline False",
+                    "Volume 30",
+                    "Brightness 20",
+                    "Contrast 20"
+                });
+        }
+
+        [Test]
+        public void UpBrightness()
         {
             TestRemoteController(new[]
                 {
@@ -50,6 +153,128 @@ namespace Refactoring
                     "IsOnline True",
                     "Volume 30",
                     "Brightness 30",
+                    "Contrast 20"
+                });
+        }
+
+        [Test]
+        public void DownBrightness()
+        {
+            TestRemoteController(new[]
+                {
+                    "Tv On",
+                    "Options change brightness down"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline True",
+                    "Volume 30",
+                    "Brightness 10",
+                    "Contrast 20"
+                });
+        }
+
+        [Test]
+        public void DontUpBrightness_When_TvIsOff()
+        {
+            TestRemoteController(new[]
+                {
+                    "Options change brightness up"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline False",
+                    "Volume 30",
+                    "Brightness 20",
+                    "Contrast 20"
+                });
+        }
+
+        [Test]
+        public void DontDownBrightness_When_TvIsOff()
+        {
+            TestRemoteController(new[]
+                {
+                    "Options change brightness down"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline False",
+                    "Volume 30",
+                    "Brightness 20",
+                    "Contrast 20"
+                });
+        }
+
+        [Test]
+        public void UpContrast()
+        {
+            TestRemoteController(new[]
+                {
+                    "Tv On",
+                    "Options change contrast up"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline True",
+                    "Volume 30",
+                    "Brightness 20",
+                    "Contrast 30"
+                });
+        }
+
+        [Test]
+        public void DownContrast()
+        {
+            TestRemoteController(new[]
+                {
+                    "Tv On",
+                    "Options change contrast down"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline True",
+                    "Volume 30",
+                    "Brightness 20",
+                    "Contrast 10"
+                });
+        }
+
+        [Test]
+        public void DontUpContrast_When_TvIsOff()
+        {
+            TestRemoteController(new[]
+                {
+                    "Options change contrast up"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline False",
+                    "Volume 30",
+                    "Brightness 20",
+                    "Contrast 20"
+                });
+        }
+
+        [Test]
+        public void DontDownContrast_When_TvIsOff()
+        {
+            TestRemoteController(new[]
+                {
+                    "Options change contrast down"
+                },
+                new[]
+                {
+                    "Options:",
+                    "IsOnline False",
+                    "Volume 30",
+                    "Brightness 20",
                     "Contrast 20"
                 });
         }
@@ -86,9 +311,9 @@ namespace Refactoring
             }, new[]
             {
                 "Options:",
-                "IsOnline True", 
-                "Volume 0", 
-                "Brightness 20", 
+                "IsOnline True",
+                "Volume 0",
+                "Brightness 20",
                 "Contrast 20"
             });
         }
@@ -101,13 +326,86 @@ namespace Refactoring
                 "Tv On",
                 "Volume Mute",
                 "Volume Unmute"
-
             }, new[]
             {
                 "Options:",
-                "IsOnline True", 
-                "Volume 30", 
-                "Brightness 20", 
+                "IsOnline True",
+                "Volume 30",
+                "Brightness 20",
+                "Contrast 20"
+            });
+        }
+        
+        [Test]
+        public void MuteVolumeAndUpVolume()
+        {
+            TestRemoteController(new[]
+            {
+                "Tv On",
+                "Volume Mute",
+                "Volume Up"
+            }, new[]
+            {
+                "Options:",
+                "IsOnline True",
+                "Volume 40",
+                "Brightness 20",
+                "Contrast 20"
+            });
+        }
+        
+        [Test]
+        public void MuteVolumeAndDownVolume()
+        {
+            TestRemoteController(new[]
+            {
+                "Tv On",
+                "Volume Mute",
+                "Volume Down"
+            }, new[]
+            {
+                "Options:",
+                "IsOnline True",
+                "Volume 20",
+                "Brightness 20",
+                "Contrast 20"
+            });
+        }
+        
+        [Test]
+        public void UpVolumeManyTimes()
+        {
+            TestRemoteController(new[]
+            {
+                "Tv On",
+                "Volume Up", "Volume Up", "Volume Up", "Volume Up", 
+                "Volume Up", "Volume Up", "Volume Up", "Volume Up", 
+                "Volume Up", "Volume Up", "Volume Up", "Volume Up",
+            }, new[]
+            {
+                "Options:",
+                "IsOnline True",
+                "Volume 100",
+                "Brightness 20",
+                "Contrast 20"
+            });
+        }
+        
+        [Test]
+        public void DownVolumeManyTimes()
+        {
+            TestRemoteController(new[]
+            {
+                "Tv On",
+                "Volume Down", "Volume Down", "Volume Down", 
+                "Volume Down", "Volume Down", "Volume Down", 
+                "Volume Down", "Volume Down", "Volume Down",
+            }, new[]
+            {
+                "Options:",
+                "IsOnline True",
+                "Volume 0",
+                "Brightness 20",
                 "Contrast 20"
             });
         }
